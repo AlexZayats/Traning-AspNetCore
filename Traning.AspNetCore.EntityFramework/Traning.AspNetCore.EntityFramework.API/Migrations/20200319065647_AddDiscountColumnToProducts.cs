b1ddbb5f26bb7f57f6529b6768e136ac4c220477
Aliaksandr Zayats <azayats@1stquad.com>
2020-03-19 10:02:11 +0300

    add discount column

---
+++ Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.API/Migrations/20200319065647_AddDiscountColumnToProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Traning.AspNetCore.EntityFramework.API.Migrations
+{
+    public partial class AddDiscountColumnToProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Discount",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "Products");
+        }
+    }
+}
--- Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.Data/Models/Product.cs
+++ Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.Data/Models/Product.cs
@@ -7,6 +7,7 @@ namespace Traning.AspNetCore.EntityFramework.Data.Models
         public Guid Id { get; set; }
         public string Name { get; set; } 
         public string Description { get; set; }
+        public float Discount { get; set; }
         public float Price { get; set; }
     }
 }
