36f5f9e4bdbe13a68c0a722785209c05c819dda3
Aliaksandr Zayats <azayats@1stquad.com>
2020-03-23 10:01:14 +0300

    add sample data

--- Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.API/ShopContextInit.cs
+++ Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.API/ShopContextInit.cs
@@ -11,9 +11,15 @@ namespace Traning.AspNetCore.EntityFramework.API
             {
                 context.Products.Add(new Product 
                 { 
-                    Name = "Cool Product",
-                    Description = "Cool product!",
-                    Price = 1000
+                    Name = "Смартфон Apple iPhone 11 64GB",
+                    Description = "Apple iOS, экран 6.1 IPS(828x1792), Apple A13 Bionic, ОЗУ 4 ГБ, флэш - память 64 ГБ, камера 12 Мп, аккумулятор 3046 мАч, 1 SIM",
+                    Price = 2300
+                });
+                context.Products.Add(new Product
+                {
+                    Name = "Смартфон Samsung Galaxy S10 G973 8GB/128GB",
+                    Description = "Android, экран 6.1 AMOLED(1440x3040), Exynos 9820, ОЗУ 8 ГБ, флэш - память 128 ГБ, карты памяти, камера 12 Мп, аккумулятор 3400 мАч, 2 SIM",
+                    Price = 1900
                 });
                 context.SaveChanges();
             }
