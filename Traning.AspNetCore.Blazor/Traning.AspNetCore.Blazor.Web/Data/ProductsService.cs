07097c13829adeb99f660c39295fd7969c342745
Aliaksandr Zayats <azayats@1stquad.com>
2020-03-25 12:57:22 +0300

    add blazor sample

---
+++ Traning.AspNetCore.Blazor/Traning.AspNetCore.Blazor.Web/Data/ProductsService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Traning.AspNetCore.EntityFramework.Logic.Models;
+
+namespace Traning.AspNetCore.Blazor.Web.Data
+{
+    public class ProductsService
+    {
+        public async Task<PagedResult<ProductDto>> GetProducts()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://localhost:44327/products");
+                if (response.IsSuccessStatusCode)
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    };
+                    var content = await response.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<PagedResult<ProductDto>>(content, options);
+                    return result;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.API/Migrations/20200323130235_AddProductType.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Traning.AspNetCore.EntityFramework.API.Migrations
+{
+    public partial class AddProductType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductReviews_Products_ProductId",
+                table: "ProductReviews");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductType",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductReviews",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductReviews_Products_ProductId",
+                table: "ProductReviews",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductReviews_Products_ProductId",
+                table: "ProductReviews");
+
+            migrationBuilder.DropColumn(
+                name: "ProductType",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductReviews",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductReviews_Products_ProductId",
+                table: "ProductReviews",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
