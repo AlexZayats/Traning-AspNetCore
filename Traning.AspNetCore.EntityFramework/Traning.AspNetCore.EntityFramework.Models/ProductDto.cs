dd34e15def9e859af87108f83689340548a04d6a
Aliaksandr Zayats <a.zayats@outlook.com>
2020-04-13 12:30:51 +0300

    add validation for blazor

--- Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.Models/ProductDto.cs
+++ Traning.AspNetCore.EntityFramework/Traning.AspNetCore.EntityFramework.Models/ProductDto.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Traning.AspNetCore.EntityFramework.Logic.Models
 {
     public class ProductDto
     {
         public Guid Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
         public float Discount { get; set; }
         public float Price { get; set; }
         public float TotalPrice => Price - Price * Discount / 100;
